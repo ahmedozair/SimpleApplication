3faba7731f427ff7436c4885b807a1fded327c53
Ahmed Ozair Chakari <x20447226@student.ncirl.ie>
2024-03-15 13:14:44 +0000

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 sudo apt udate && sudo apt install nodejs npm
 sudo npm install -g pm2
 pm2 stop simple_app
-cd /SimpleApplication
+cd SimpleApplication/
 npm install
 pm2 start ./bin/www --name simple_app
